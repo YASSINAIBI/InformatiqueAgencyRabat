3d0433511ca67f8b18ee374ee57ae2eb23da3c34
yassineaibi <aibiyassin8@gmail.com>
2020-12-24 21:53:22 +0100

    informatique Agency Rabat

---
+++ include/navbar.php
@@ -0,0 +1,73 @@
+<!-- <section class="NavbarT">
+    
+</section> -->
+
+<section class="NavbarT">
+    <nav class="navbar navbar-expand-lg navbar-dark">
+        <div class="collapse navbar-collapse container">
+            <!-- id="navbarSupportedContent" -->
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item mr-n2">
+                    <a class="nav-link" href="#"><i class="fas fa-phone-alt"></i></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"> 01 47 93 32 75</a>
+                </li>
+                <li class="nav-item p-0" style="font-size: 20px;">
+                    <a class="nav-link p-1" href="#"><i class="fas fa-envelope"></i></a>
+                </li>
+                <li class="nav-item p-0" style="font-size: 20px;">
+                    <a class="nav-link p-1" href="#"><i class="fab fa-linkedin"></i></a>
+                </li>
+            </ul>
+            <ul class="navbar-nav mt-2">
+                <li class="nav-item">
+                    <a class="leftContent">Partenaires <span><i class="fas fa-angle-down"></i></span> </a>
+
+                    <ul style="position: absolute;" class="drop-down">
+                        <li>Dell</li>
+                        <li>Eset</li>
+                        <li>F-Secure</li>
+                        <li>HP</li>
+                        <li>Lenovo</li>
+                        <li>Watchguard</li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="leftContent">Qui sommes-nous</a>
+                </li>
+                <li class="nav-item">
+                    <a class="leftContent">Blog</a>
+                </li>
+                <li class="nav-item">
+                    <a class="leftContent">Client <span><i class="fas fa-angle-down"></i></span> </a>
+
+                    <ul style="position: absolute;" class="drop-down">
+                        <li>Espace client</li>
+                        <li>Assistance</li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="leftContent">Nos métiers <span><i class="fas fa-angle-down"></i></span> </a>
+
+                    <ul style="position: absolute;" class="drop-down">
+                        <li>Administrateur systeme et réseau</li>
+                        <li>chef de projet informatique</li>
+                        <li>Technicien hotline</li>
+                        <li>Technicien systeme et réseau</li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="leftContent">Nous rejoindre</a>
+                </li>
+                <li class="nav-item">
+
+                </li>
+                <li class="nav-item">
+                    <i class="fas fa-search" style="color: white;"></i>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <!--Main Navigation-->
+</section>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php include 'include/head.php' ?>
+
+<?php include 'include/navbar.php' ?>
+
+
+
+<?php include 'include/footer.php' ?>
+
+<?php include 'include/allscript.php' ?>
