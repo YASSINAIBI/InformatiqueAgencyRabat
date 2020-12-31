4900e02eccd5bc8e581e9b5907c3ff8f0b2193fc
GITSALAHE <salah.bouanba2@gmail.com>
2020-12-31 05:28:10 +0100

    done

---
+++ index.php
@@ -0,0 +1,307 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="Description" content="Enter your description here" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+    <link rel="stylesheet">
+    <title>Title</title>
+</head>
+
+<body>
+    <header>
+
+        <div class="container-fluid nav-small d-none d-xl-block text-white">
+            <div class="container d-flex justify-content-between align-items-center">
+                <div class="ste-info">
+                    ARESCOM - Solutions informatiques pour entreprises
+                </div>
+
+                <div>
+                    <div class="d-flex align-items-center">
+                        <li class="navItems">Assistance à distance</li>
+                        <li class="navItems">Espace client</li>
+                        <li class="navPhonePhoto"><img src="img/phone.jpg" alt=""></li>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <div class="container">
+            <nav class="navbar navbar-expand-lg navbar-light bg-transparent text-light">
+                <a class="navbar-brand text-white" href="#"><img height="100" width="300        " src="img/logo.png" alt=""></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item active">
+                            <a class="nav-link text-white" href="#">Acceuil</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="#">Pack maintenance</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="#">Solution</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="#">Solution</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="#">Recrutement</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="#">CONTACT</a>
+                        </li>
+                        <li class="nav-item">
+
+                            <button type="button" class="btn btn-warning text-white"> <i class="fa fa-calculator "></i> Warning</button>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+
+
+    </header>
+
+    <section class="videoSection">
+        <iframe class="iframeVideo" src="https://player.vimeo.com/video/380003977?background=1&autoplay=1&loop=1&byline=0&title=0" frameborder="0"></iframe>
+    </section>
+
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3 cardModed">
+                <div class="card">
+                    <div class="view overlay hm-white-slight cardBorder">
+                        <a class="icone"><i class="fa fa-tools"></i></a>
+                        <h2 class="text-center text-card">Maintenance informatique des PME</h2>
+                        <p class="text-center">Maintenance intégrale de votre système informatique et assistance technique quotidienne.</p>
+
+                        <a href="#!">
+                            <div class="mask"></div>
+                        </a>
+                    </div>
+                    <div class="">
+                        <img class="card-img" src="img/1.jpg" alt="">
+                    </div>
+                    <div class="card-footer cardFooter">
+                        <span>En savoir plus</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 cardModed">
+                <div class="card">
+                    <div class="view overlay hm-white-slight cardBorder">
+                        <a class="icone"><i class="fa fa-tools"></i></a>
+                        <h2 class="text-center text-card">Maintenance informatique des PME</h2>
+                        <p class="text-center">Maintenance intégrale de votre système informatique et assistance technique quotidienne.</p>
+
+                        <a href="#!">
+                            <div class="mask"></div>
+                        </a>
+                    </div>
+                    <div class="">
+                        <img class="card-img" src="img/1.jpg" alt="">
+                    </div>
+                    <div class="card-footer cardFooter">
+                        <span>En savoir plus</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 cardModed">
+                <div class="card">
+                    <div class="view overlay hm-white-slight cardBorder">
+                        <a class="icone"><i class="fa fa-tools"></i></a>
+                        <h2 class="text-center text-card">Maintenance informatique des PME</h2>
+                        <p class="text-center">Maintenance intégrale de votre système informatique et assistance technique quotidienne.</p>
+
+                        <a href="#!">
+                            <div class="mask"></div>
+                        </a>
+                    </div>
+                    <div class="">
+                        <img class="card-img" src="img/1.jpg" alt="">
+                    </div>
+                    <div class="card-footer cardFooter">
+                        <span>En savoir plus</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 cardModed">
+                <div class="card">
+                    <div class="view overlay hm-white-slight cardBorder">
+                        <a class="icone"><i class="fa fa-tools"></i></a>
+                        <h2 class="text-center text-card">Maintenance informatique des PME</h2>
+                        <p class="text-center">Maintenance intégrale de votre système informatique et assistance technique quotidienne.</p>
+
+                        <a href="#!">
+                            <div class="mask"></div>
+                        </a>
+                    </div>
+                    <div class="">
+                        <img class="card-img" src="img/1.jpg" alt="">
+                    </div>
+                    <div class="card-footer cardFooter">
+                        <span>En savoir plus</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 cardModed">
+                <div class="card">
+                    <div class="view overlay hm-white-slight cardBorder">
+                        <a class="icone"><i class="fa fa-tools"></i></a>
+                        <h2 class="text-center text-card">Maintenance informatique des PME</h2>
+                        <p class="text-center">Maintenance intégrale de votre système informatique et assistance technique quotidienne.</p>
+
+                        <a href="#!">
+                            <div class="mask"></div>
+                        </a>
+                    </div>
+                    <div class="">
+                        <img class="card-img" src="img/1.jpg" alt="">
+                    </div>
+                    <div class="card-footer cardFooter">
+                        <span>En savoir plus</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 cardModed">
+                <div class="card">
+                    <div class="view overlay hm-white-slight cardBorder">
+                        <a class="icone"><i class="fa fa-tools"></i></a>
+                        <h2 class="text-center text-card">Maintenance informatique des PME</h2>
+                        <p class="text-center">Maintenance intégrale de votre système informatique et assistance technique quotidienne.</p>
+
+                        <a href="#!">
+                            <div class="mask"></div>
+                        </a>
+                    </div>
+                    <div class="">
+                        <img class="card-img" src="img/1.jpg" alt="">
+                    </div>
+                    <div class="card-footer cardFooter">
+                        <span>En savoir plus</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 cardModed">
+                <div class="card">
+                    <div class="view overlay hm-white-slight cardBorder">
+                        <a class="icone"><i class="fa fa-tools"></i></a>
+                        <h2 class="text-center text-card">Maintenance informatique des PME</h2>
+                        <p class="text-center">Maintenance intégrale de votre système informatique et assistance technique quotidienne.</p>
+
+                        <a href="#!">
+                            <div class="mask"></div>
+                        </a>
+                    </div>
+                    <div class="">
+                        <img class="card-img" src="img/1.jpg" alt="">
+                    </div>
+                    <div class="card-footer cardFooter">
+                        <span>En savoir plus</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 cardModed">
+                <div class="card">
+                    <div class="view overlay hm-white-slight cardBorder">
+                        <a class="icone"><i class="fa fa-tools"></i></a>
+                        <h2 class="text-center text-card">Maintenance informatique des PME</h2>
+                        <p class="text-center">Maintenance intégrale de votre système informatique et assistance technique quotidienne.</p>
+
+                        <a href="#!">
+                            <div class="mask"></div>
+                        </a>
+                    </div>
+                    <div class="">
+                        <img class="card-img" src="img/1.jpg" alt="">
+                    </div>
+                    <div class="card-footer cardFooter">
+                        <span>En savoir plus</span>
+                    </div>
+                </div>
+            </div>
+
+            <!--row-->
+        </div>
+    </div>
+
+    <div class="container-fluid containerModed">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-7">
+                    <h2 class="textModed">
+                        LA PERFORMANCE DE VOTRE INFORMATIQUE D’ENTREPRISE AU COEUR DE NOTRE MISSION</h2>
+                    <blockquote>
+                        <h3 class="text-left" style="font-size:20px">Mettre en place, maintenir, gérer et optimiser votre infrastructure informatique afin que celle-ci soit 100% disponible pour le travail de vos collaborateurs au quotidien.</h3>
+                    </blockquote>
+                    <p>Pour ce faire ARESCOM met à votre disposition des outils technologiques performants et une équipe complète de professionnels de l’informatique (techniciens hotline, techniciens d’intervention, administrateurs systèmes et réseau et ingénieurs.</p>
+                    <img src="img/tablet.png" style="width: 100%;" alt="">
+                </div>
+
+                <div class="col-lg col-md-5 formulaire">
+                    <div class="d-flex justify-content-center">
+                        <img src="img/calc.png" alt="">
+                        </div> 
+                        <h3 class="text-white formText">Un besoin urgent ? Demandez votre devis gratuit et sans engagement</h3>
+
+                        <form action="" method="post">
+                            <div class="form-row">
+                                <div class="form-group col-12">
+                                    <label for=""></label>
+                                    <input type="text" name="" id="" class="form-control" placeholder="" aria-describedby="helpId">
+                                </div>
+                                <div class="form-group col-12">
+                                    <label for=""></label>
+                                    <input type="text" name="" id="" class="form-control" placeholder="" aria-describedby="helpId">
+                                </div>
+                                <div class="form-group col-md-6">
+                                    <label for=""></label>
+                                    <input type="text" name="" id="" class="form-control" placeholder="" aria-describedby="helpId">
+                                </div>
+                                <div class="form-group col-md-6">
+                                    <label for=""></label>
+                                    <input type="text" name="" id="" class="form-control" placeholder="" aria-describedby="helpId">
+                                </div>
+                                <div class="form-group col-md-6">
+                                    <label for=""></label>
+                                    <input type="text" name="" id="" class="form-control" placeholder="" aria-describedby="helpId">
+                                </div>
+                                <div class="form-group col-md-6">
+                                    <label for=""></label>
+                                    <input type="text" name="" id="" class="form-control" placeholder="" aria-describedby="helpId">
+                                </div>
+                                <button type="submit" class="btn btn-primary col">Submit</button>
+                            </div>
+
+
+                        </form>
+                            <p class="text-white">Vous souhaitez apporter plus de détails ? Cliquez ici !</p>
+                        <div class="form-check">
+                            <label class="form-check-label" style="color:#a3b8c7">
+                                <input type="checkbox" class="form-check-input" name="" id="" value="checkedValue" checked>
+                                J'accepte que mes informations soient utilisées pour ma demande et la relation commerciale qui pourrait en découler.
+
+                                Pour connaître et exercer vos droits, notamment de retrait de votre consentement à l’utilisation des données collectées par ce formulaire, veuillez consulter notre
+                            </label>
+                        </div>
+                   
+
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.1/umd/popper.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.3/js/bootstrap.min.js"></script>
+</body>
+
+</html>
